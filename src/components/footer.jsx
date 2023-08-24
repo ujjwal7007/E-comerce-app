6ea652eaf1e31e1fa87812dcfcdff9724127fe43
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-24 15:10:01 +0530

    images

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import {AiFillTwitterCircle} from "react-icons/ai"
 import {BsFacebook, BsInstagram, BsLinkedin} from "react-icons/bs"
+import google from "../assets/images/pay/play.jpg"
 
 const footer = () => {
   return (
@@ -10,7 +11,7 @@ const footer = () => {
       <div className="container-xxl">
         <div className="row">
           <div className='col-4 d-flex flex-column'>
-            <h3 className='mb-5'>Contact us</h3>
+            <h3 className='mb-4'>Contact us</h3>
             <div className="footer-details">
               <p className='mb-3'><b>Address:</b> Ground floor, Mohan Rd, near Tikait Rai Talab, opp. Power House Road, LDA Colony, Rajajipuram, Lucknow, Uttar Pradesh 226003</p>
               <p className='mb-3'><b>Phone:</b><a href="tel:+9157865438"> Call us +9157865438</a></p>
@@ -24,16 +25,67 @@ const footer = () => {
               </div>
             </div>
           </div>
+          <div className="col-2">
+            <h3 className="mb-4">About</h3>
+            <div className="footer-details d-flex flex-column">
+              <Link className='mb-3 text-black'>
+                About Us
+              </Link>
+              <Link className='mb-3 text-black'>
+                Delivery
+              </Link>
+              <Link className='mb-3 text-black'>
+                Privacy Policy
+              </Link>
+              <Link className='mb-3 text-black'>
+                Terms & Conditions
+              </Link>
+              <Link className='mb-3 text-black'>
+                Fee Policy
+              </Link>
+            </div>
+          </div>
+          <div className="col-2">
+            <h3 className="mb-4">Account</h3>
+            <div className="footer-details d-flex flex-column">
+              <Link className='mb-3 text-black'>
+                Profile
+              </Link>
+              <Link className='mb-3 text-black'>
+                View Cart
+              </Link>
+              <Link className='mb-3 text-black'>
+                Help
+              </Link>
+              <Link className='mb-3 text-black'>
+                Payments
+              </Link>
+              <Link className='mb-3 text-black'>
+                My Wishlist
+              </Link>
+              <Link className='mb-3 text-black'>
+                Coupons
+              </Link>
+            </div>
+          </div>
+          <div className="col-4">
+            <h3 className='mb-3'>Install App</h3>
+            <div className="footer-details">
+              <p>Available On Google Play Services & App Store</p>
+              <img src={google} alt='' className='img-fluid p-4' />
+            </div>
+
+          </div>
         </div>
         <hr/>
-        <div className="row">
-          <div className="col-9">
+        <div className="row d-flex justify-content-between">
+          <div className="col-3">
             <p>&copy; Developed by Ujjwal Gupta 2023</p>
           </div>
-          <div className="col-3 d-flex justify-content-between">
-            <Link>Privacy Policy</Link>
-            <Link>Terms of Use</Link>
-            <Link>Contact Me</Link>
+          <div className="col-3 d-flex justify-content-around">
+            <Link className='text-black'>Privacy Policy</Link>
+            <Link className='text-black'>Terms of Use</Link>
+            <Link className='text-black'>Contact Me</Link>
           </div>
         </div>
       </div>
