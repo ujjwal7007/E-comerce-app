f5de455f07dd853c73ecd8d80ab6b7ffa36640c9
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-24 10:35:36 +0530

    footer start

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <>
+    <footer className='footer p-5'>
+      <div className="container-xxl">
+        <div className="orw">
+          <div className='col-4 d-flex flex-column'>
+            <h3 className='mb-5'>Contact us</h3>
+            <div className="footer-details">
+              <p><b>Address:</b> Ground floor, Mohan Rd, near Tikait Rai Talab, opp. Power House Road, LDA Colony, Rajajipuram, Lucknow, Uttar Pradesh 226003</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    </>
   )
 }
 
