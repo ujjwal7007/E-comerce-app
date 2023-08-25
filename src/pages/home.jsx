47d48fadedf0e666fd8812499759581c173d6888
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-25 10:47:00 +0530

    home banner section create

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <>
+    <section className='banner p-5'>
+      <div className="container-xxl">
+        <div className="row">
+          <div className="banner-details">
+            <span className='shadow-lg p-3'>Trade in offer!!</span>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
