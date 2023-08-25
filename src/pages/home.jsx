e57cf742e9081818401ec81709fa8531eff0ae3b
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-25 11:39:40 +0530

    banner text

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,8 +6,9 @@ const home = () => {
     <section className='banner p-5'>
       <div className="container-xxl">
         <div className="row">
-          <div className="banner-details">
-            <span className='shadow-lg p-3'>Trade in offer!!</span>
+          <div className="banner-details d-flex flex-column align-items-center justify-content-center">
+            <span className='shadow-lg p-3 text-white'>Trade in offer!!</span>
+            <h1>Super Value Deals</h1>
           </div>
         </div>
       </div>
