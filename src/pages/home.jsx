d5ebf6c9bc629ff56321dff10a18b19a1458d7bb
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-28 22:23:56 +0530

    section deals

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Hero from "../components/hero";
 import FeaturedProducts from "../components/featuredProducts";
-import NewArrival from "../components/newArrival"
+import NewArrival from "../components/newArrival";
+import img1 from "../assets/images/banner/b17.jpg";
+import img2 from "../assets/images/banner/b10.jpg";
 
 const home = () => {
   return (
@@ -65,6 +67,45 @@ const home = () => {
               <p>Your Best Designer Outfits</p>
             </div>
             <NewArrival />
+          </div>
+        </div>
+      </section>
+
+      <section className="deals p-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="card row">
+                <div className="d-flex">
+                  <div className="col-md-4">
+                    <img
+                      src={img1}
+                      alt=""
+                      className="img-fluid rounded-start"
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <span>Hot Deals</span>
+                      <h3>Buy One Get One Free.</h3>
+                      <p>
+                        The latest best collection in our closet feel cute with
+                        our outfits
+                      </p>
+                      <button className="button-deals mt-3">Learn More</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-6">
+              <div className="card">
+                <div className="col-md-4">
+                  <img src={img2} alt="" className="img-fluid rounded-start" />
+                </div>
+                <div className="col-md-8"></div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
