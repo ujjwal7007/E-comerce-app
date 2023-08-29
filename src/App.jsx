29923f846f73c5921f72f43de9ac099058723f9d
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-29 23:10:50 +0530

    shop details

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,38 @@
-import './App.css'
-import Layout from './components/layout'
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import Home from './pages/home'
-import Shop from './pages/shop'
-import Blog from './pages/blog'
-import About from "./pages/about"
-import Content from './pages/content'
-import Cart from "./pages/cart"
-import Login from "./pages/login"
-import Wishlist from "./pages/wishlist"
+import "./App.css";
+import Layout from "./components/layout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Shop from "./pages/shop";
+import Blog from "./pages/blog";
+import About from "./pages/about";
+import Content from "./pages/content";
+import Cart from "./pages/cart";
+import Login from "./pages/login";
+import Wishlist from "./pages/wishlist";
+
+import { ShopContext } from "./components/shopcontext";
 
 function App() {
-  
-
   return (
     <>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<Home/>}/>
-          <Route path='shop' element={<Shop/>}/>
-          <Route path='blog' element={<Blog/>}/>
-          <Route path='about' element={<About/>}/>
-          <Route path='content' element={<Content/>}/>
-          <Route path='cart' element={<Cart/>}/>
-          <Route path='login' element={<Login/>}/>
-          <Route path='wishlist' element={<Wishlist/>}/>
-        </Route>
-      </Routes>
-      </BrowserRouter>
+      <ShopContext>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="shop" element={<Shop />} />
+              <Route path="blog" element={<Blog />} />
+              <Route path="about" element={<About />} />
+              <Route path="content" element={<Content />} />
+              <Route path="cart" element={<Cart />} />
+              <Route path="login" element={<Login />} />
+              <Route path="wishlist" element={<Wishlist />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </ShopContext>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/prod.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const prod = () => {
+    const {id, name, image, price, brand} = props.data
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default prod
