d8db6a2486f30ff38b1a768587f155ff3d030c74
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-08-29 22:15:16 +0530

    create shop

--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const shop = () => {
   return (
-    <div>shop</div>
-  )
-}
+    <>
+      <section className="repair">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="repair-details text-center d-flex flex-column align-items-center p-3">
+              <span className="text-white mt-5">Repair Services</span>
+              <h2 className="text-white">
+                On{" "}
+                <span className="text-info">
+                  <b>70%</b>
+                </span>{" "}
+                off on All Products And Accessories
+              </h2>
+              <div className="mt-3">
+                <button className="button-repair">Explore Now</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default shop
+export default shop;
