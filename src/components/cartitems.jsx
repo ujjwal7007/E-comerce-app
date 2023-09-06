2c7bf9b884735f9fb2b3f7709edb6cbeda10b369
Ujjwal Gupta <ujjwalgupta2511@gmail.com>
2023-09-06 22:43:30 +0530

    cartitems editing

--- src/components/cartitems.jsx
+++ src/components/cartitems.jsx
@@ -8,78 +8,60 @@ const cartitems = (props) => {
     viewProductDetails,
     addToCart,
     cartItems,
-    removeTocart,
+    removeToCart,
     updateCartItemCount,
   } = useContext(ShopContext);
   return (
     <>
-      <section className="card-item p-4">
-        <div className="container-xxl">
-          <div key={id} className="row">
-            <table>
-              <thead className="mb-3">
-                <th className="col-3">Product Image</th>
-                <th className="col-3">Product Details</th>
-                <th className="col-3">Edit</th>
-                <th className="col-2">Coupons</th>
-              </thead>
-              <tbody className="mb-3">
-                <tr>
-                  <td className="col-3">
-                    <img src={image} alt="" className="img-fluid" />
-                  </td>
-                  <td className="col-3">
-                    <p>{brand}</p>
-                    <p>{name}</p>
-                    <p>{price}</p>
-                  </td>
-                  <td>
-                    <button
-                      className="add-btn"
-                      onClick={() => {
-                        addToCart(id);
-                        event.target.classList.toggle("text-success");
-                      }}
-                    >
-                      +
-                    </button>
-                    <input
-                      type="text"
-                      value={cartItems[id]}
-                      onChange={(e) =>
-                        updateCartItemCount(Number(e.target.value), id)
-                      }
-                    />
-                    <button
-                      className="add-btn"
-                      onClick={() => {
-                        removeTocart(id);
-                        event.target.classList.toggle("text-success");
-                      }}
-                    >
-                      -
-                    </button>
-                  </td>
-                  <td>
-                    <div className="input-group mb-3">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Apply coupon code.."
-                        aria-label="Apply coupon code"
-                        aria-describedby="basic-addon2"
-                      />
-                      <span className="input-group-text" id="basic-addon2">
-                        Search
-                      </span>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </section>
+      <tbody key={id}>
+        <tr>
+          <td>
+            <img src={image} alt="" className="img-fluid" />
+          </td>
+          <td>
+            <p>{brand}</p>
+            <p>{name}</p>
+            <p>{price}</p>
+          </td>
+          <td>
+            <button
+              className="add-btn"
+              onClick={() => {
+                addToCart(id);
+              }}
+            >
+              +
+            </button>
+            <input
+              type="text"
+              value={cartItems[id]}
+              onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+            />
+            <button
+              className="add-btn"
+              onClick={() => {
+                removeToCart(id);
+              }}
+            >
+              -
+            </button>
+          </td>
+          <td>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Apply coupon code.."
+                aria-label="Apply coupon code"
+                aria-describedby="basic-addon2"
+              />
+              <span className="input-group-text" id="basic-addon2">
+                Search
+              </span>
+            </div>
+          </td>
+        </tr>
+      </tbody>
     </>
   );
 };
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,22 +1,33 @@
-import React, { useContext } from 'react'
-import { PRODUCTS, PRODUCTS1 } from '../components/products'
-import Cartitems from '../components/cartitems'
-import { ShopContext } from '../components/shopcontext';
-
+import React, { useContext } from "react";
+import { PRODUCTS, PRODUCTS1 } from "../components/products";
+import Cartitems from "../components/cartitems";
+import { ShopContext } from "../components/shopcontext";
 
 const cart = () => {
   const { viewProductDetails, addToCart, cartItems } = useContext(ShopContext);
   return (
     <>
-    <div className="row">
-        {[...PRODUCTS, ...PRODUCTS1].map((product) => {
-          if(cartItems[product.id] !== 0)  {
-              return<Cartitems Key={product.id} data={product}/>
-            }
-        })}
-    </div>
+      <section className="card-item p-4">
+        <div className="container-xxl">
+          <div className="row">
+            <table>
+              <thead className="my-2">
+                <th className="col-3">Product Image</th>
+                <th className="col-3">Product Details</th>
+                <th className="col-3">Edit</th>
+                <th className="col-2">Coupons</th>
+              </thead>
+                {[...PRODUCTS, ...PRODUCTS1].map((product) => {
+                  if (cartItems[product.id] !== 0) {
+                    return <Cartitems Key={product.id} data={product} />;
+                  }
+                })}
+            </table>
+          </div>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default cart
+export default cart;
